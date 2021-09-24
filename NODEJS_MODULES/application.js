0e78a411b0010d1fbc9040a6b2c2c01754399990
Saba-26 <83002906+Saba-26@users.noreply.github.com>
2021-09-25 03:38:06 +0530

    asyn function

--- NODEJS_MODULES/application.js
+++ NODEJS_MODULES/application.js
@@ -1,16 +1,38 @@
-const fs=require('fs')
+// const fs=require('fs')
 
-console.log('Start Sync');
-const file=fs.readdirSync('./')
-console.log(`Files:${file}`);
-console.log('End Sync');
+// console.log('Start Sync');
+// const file=fs.readdirSync('./')
+// console.log(`Files:${file}`);
+// console.log('End Sync');
 
 
-console.log("-----------------ASYNC-----------------");
-console.log('Start Async');
-const fileAsync=fs.readdir('./',(err,files)=>{ //call back function
-    if(err) console.log(err);
-    else console.log(files);
-})
-console.log('End Async');
+// console.log("-----------------ASYNC-----------------");
+// console.log('Start Async');
+// const fileAsync=fs.readdir('./',(err,files)=>{ //call back function
+//     if(err) console.log(err);
+//     else console.log(files);
+// })
+// console.log('End Async');
 
+// fs.mkdir('./New',(err)=>{
+//     if(err)console.log(err);
+//     else console.log('Directory Created!');
+// })
+
+// fs.rename('./New','./Latest',(err)=>{
+//     if(err)console.log(err);
+//     else console.log('Successfully Renamed!');
+// })
+
+const os = require('os')
+
+freeMemory=os.freemem();
+console.log(`Free Memory: ${freeMemory}`);
+
+
+totalMemory=os.totalmem();
+console.log(`Total Memory: ${totalMemory}`);
+
+console.log(os.homedir());
+
+console.log(os.hostname());
